988733814701f2f4d8631680afd64263b98053e3
Prathwik <64214685+prathwik0@users.noreply.github.com>
2024-02-16 17:57:41 +0530

    feat: static assets

---
+++ static/theme.js
@@ -0,0 +1,7 @@
+if (localStorage.getItem('theme') === 'dark') {
+	document.documentElement.classList.add('dark');
+} else if (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	document.documentElement.classList.add('dark');
+} else {
+	document.documentElement.classList.remove('dark');
+}
