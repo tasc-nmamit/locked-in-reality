146f95a39982eea9f379447ce04c900b3b0257e7
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2024-02-18 19:56:01 +0530

    feat: copy paste core

--- src/routes/serverRoom/bootMode/+page.svelte
+++ src/routes/serverRoom/bootMode/+page.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 	import BouncingDots from '../../chat/BouncingDots.svelte';
-	
-    let active = 0;
-    let bootStatus = "Automatic";
-    let focusOn = "textarea";
-    let textareaElement:HTMLTextAreaElement;
-    let inputElement:HTMLInputElement;
 
-    const changeFocus = () => {
-        if (focusOn === "textarea") {
-            focusOn = "authinput";
-            inputElement.focus();
-        } else {
-            focusOn = "textarea";
-            textareaElement.focus();
-        }
-    }
+	let active = 0;
+	let bootStatus = 'Automatic';
+	let focusOn = 'textarea';
+	let textareaElement: HTMLTextAreaElement;
+	let inputElement: HTMLInputElement;
+	let textareaValue = '';
 
-    onMount(() => {
-        document.addEventListener('keydown', function(event) {
-            if (event.key === 'Tab') {
-                event.preventDefault(); // Prevent default tab behavior
-                changeFocus();
-            } else if (event.key === 'Escape') {
-                event.preventDefault();
-                window.location.href = '/serverRoom';
-            }
-        })
-    })
-/*
+	function checkForSuccess() {
+		const desiredString = `void runFn(){
+    doSomething();
+    return null;
+}`;
+		if (textareaValue.trim() === desiredString.trim()) {
+			alert('Successful!');
+		}
+	}
+
+	const changeFocus = () => {
+		if (focusOn === 'textarea') {
+			focusOn = 'authinput';
+			inputElement.focus();
+		} else {
+			focusOn = 'textarea';
+			textareaElement.focus();
+		}
+	};
+
+	onMount(() => {
+		document.addEventListener('keyup', function (event) {
+			if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+				event.preventDefault(); // Prevent default tab behavior
+				changeFocus();
+			} else if (event.key === 'Escape') {
+				event.preventDefault();
+				window.location.href = '/serverRoom';
+			}
+		});
+	});
+	/*
 
 		function updateActiveItem() {
 			const items = document.querySelectorAll('.boot-options p');
@@ -52,61 +63,69 @@
 		</div>
 	</header>
 	<body class="flex h-[74%] w-full flex-wrap bg-inherit text-3xl font-[600] tracking-normal text-black">
-        <div class="w-full h-full">
-            <div class="w-full flex flex-row justify-evenly mt-3">
-                <h3>Boot status :</h3>
-                <h3>{bootStatus}</h3>
-            </div>
-            <div class="w-full flex justify-center my-8">
-                <h2>To unlock manual <u>Run the code</u></h2>
-            </div>
-            <div class="w-full h-auto flex flex-col justify-center items-center">
-                <textarea id="terminal" cols="30" rows="14" bind:this={textareaElement} class="w-3/4 h-3/4 bg-black text-gray-200 resize-none"></textarea>
-            </div>
-            <!-- auth code -->
-            <div class="w-full flex flex-row justify-evenly mt-12">
-                <h3>Auth signature :</h3>
-                <h3><input type="text" bind:this={inputElement} class="bg-[#9c9a9d]"/></h3>
-            </div>
-        </div>
+		<div class="h-full w-full">
+			<div class="mt-3 flex w-full flex-row justify-evenly">
+				<h3>Boot status : {bootStatus}</h3>
+			</div>
+			<div class="my-8 flex w-full justify-center">
+				<h2>To unlock manual <u>Run the code</u></h2>
+			</div>
+			<div class="flex h-auto w-full flex-col items-center justify-center">
+				<textarea id="terminal" cols="30" rows="14" bind:value={textareaValue} bind:this={textareaElement} on:input={checkForSuccess} class="h-3/4 w-2/4 resize-none bg-black font-normal"></textarea>
+			</div>
+			<!-- auth code -->
+			<div class="mt-12 flex w-full flex-row justify-center">
+				<h3>Auth signature :</h3>
+				<h3><input type="text" bind:this={inputElement} class="bg-black px-2 py-1 font-normal text-white" /></h3>
+			</div>
+		</div>
 	</body>
 	<footer class="flex h-[16%] w-full items-center justify-center border bg-[#000069] text-white">
 		<div class="flex h-32 w-[99%] items-center justify-center border border-x-4 border-white">
-			<div class="flex h-[65%] w-full flex-col gap-x-96 bg-black px-6 text-nowrap">
-                <div class="flex flex-row justify-between w-full mb-1">
-                    <p class="text-3xl tracking-widest">&#x2191&#x2193=Change line</p>
-                    <p class="text-3xl tracking-widest">&ltEnter&gt=Complete Entry</p>
-                    <p class="text-3xl tracking-widest">ESC=Exit Entry</p>
-                </div>
-                <div class="flex flex-row justify-between w-full">
-                    <p class="text-3xl tracking-widest">&ltTab&gt=Move Highlight</p>
-                    <p></p>
-                    <p></p>
-                </div>
+			<div class="flex h-[65%] w-full items-end justify-start gap-x-96 bg-black px-6">
+				<p class="text-3xl tracking-widest">&#x2191&#x2193=Move Highlight</p>
+				<p class="text-3xl tracking-widest">&ltEnter&gt=Complete Entry</p>
+				<p class="text-3xl tracking-widest">ESC=Exit Entry</p>
 			</div>
 		</div>
 	</footer>
+	<!-- <footer class="flex h-[16%] w-full items-center justify-center border bg-[#000069] text-white">
+		<div class="flex h-32 w-[99%] items-center justify-center border border-x-4 border-white">
+			<div class="flex h-[65%] w-full flex-col gap-x-96 text-nowrap bg-black px-6">
+				<div class="mb-1 flex w-full flex-row justify-between">
+					<p class="text-3xl tracking-widest">&#x2191&#x2193=Change line</p>
+					<p class="text-3xl tracking-widest">&ltEnter&gt=Complete Entry</p>
+					<p class="text-3xl tracking-widest">ESC=Exit Entry</p>
+				</div>
+				<div class="flex w-full flex-row justify-between">
+					<p class="text-3xl tracking-widest">&ltTab&gt=Move Highlight</p>
+					<p></p>
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</footer> -->
 </section>
 
 <style>
-    textarea{
-        caret: white;
-    }
-    input{
-        caret: black;
-        outline: none;
-    }
+	textarea {
+		caret: white;
+	}
+	input {
+		caret: black;
+		outline: none;
+	}
 
-    #terminal {
-        background-color: #000000;
-        color: #eeeeec;
-        border: none;
-        padding: 10px;
-        overflow: auto;
-        scrollbar-width: none;
-    }
+	#terminal {
+		background-color: #000000;
+		color: #eeeeec;
+		border: none;
+		padding: 10px;
+		overflow: auto;
+		scrollbar-width: none;
+	}
 
-    textarea::-webkit-scrollbar {
-        display: none; /* For Chrome, Safari and Opera */
-    }
+	textarea::-webkit-scrollbar {
+		display: none; /* For Chrome, Safari and Opera */
+	}
 </style>
