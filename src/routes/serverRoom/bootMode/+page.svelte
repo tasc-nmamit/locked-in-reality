2407b03cc1f2b6d5174278ce5bb8f5138c20e988
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2024-02-19 15:37:55 +0530

    fix: footer fix

--- src/routes/serverRoom/bootMode/+page.svelte
+++ src/routes/serverRoom/bootMode/+page.svelte
@@ -19,28 +19,28 @@
 		}
 	}
 
-    const changeFocus = () => {
-        if (focusOn === "textarea") {
-            focusOn = "authinput";
-            inputElement.focus();
-        } else {
-            focusOn = "textarea";
-            textareaElement.focus();
-        }
-    }
+	const changeFocus = () => {
+		if (focusOn === 'textarea') {
+			focusOn = 'authinput';
+			inputElement.focus();
+		} else {
+			focusOn = 'textarea';
+			textareaElement.focus();
+		}
+	};
 
-    onMount(() => {
-        document.addEventListener('keydown', function(event) {
-            if (event.key === 'Tab') {
-                event.preventDefault(); // Prevent default tab behavior
-                changeFocus();
-            } else if (event.key === 'Escape') {
-                event.preventDefault();
-                window.location.href = '/serverRoom';
-            }
-        })
-    })
-/*
+	onMount(() => {
+		document.addEventListener('keyup', function (event) {
+			if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+				event.preventDefault(); // Prevent default tab behavior
+				changeFocus();
+			} else if (event.key === 'Escape') {
+				event.preventDefault();
+				window.location.href = '/serverRoom';
+			}
+		});
+	});
+	/*
 
 		function updateActiveItem() {
 			const items = document.querySelectorAll('.boot-options p');
@@ -63,39 +63,23 @@
 		</div>
 	</header>
 	<body class="flex h-[74%] w-full flex-wrap bg-inherit text-3xl font-[600] tracking-normal text-black">
-		<div class="h-full w-full">
-			<div class="mt-3 flex w-full flex-row justify-evenly">
+		<div class="flex h-full w-full flex-col">
+			<div class="flex w-full flex-row justify-evenly">
 				<h3>Boot status : {bootStatus}</h3>
 			</div>
-			<div class="my-8 flex w-full justify-center">
+			<div class="flex w-full justify-center">
 				<h2>To unlock manual <u>Run the code</u></h2>
 			</div>
 			<div class="flex h-auto w-full flex-col items-center justify-center">
-				<textarea id="terminal" cols="30" rows="14" bind:value={textareaValue} bind:this={textareaElement} on:input={checkForSuccess} class="h-3/4 w-2/4 resize-none bg-black font-normal cursor-none"></textarea>
+				<textarea id="terminal" cols="30" rows="14" bind:value={textareaValue} bind:this={textareaElement} on:input={checkForSuccess} class="h-3/4 w-2/4 resize-none bg-black font-normal"></textarea>
 			</div>
 			<!-- auth code -->
-			<div class="mt-12 flex w-full flex-row justify-center">
+			<div class="flex w-full flex-row items-center justify-center">
 				<h3>Auth signature :</h3>
-				<h3><input type="text" bind:this={inputElement} class="bg-black px-2 py-1 font-normal text-white cursor-none" /></h3>
+				<input type="text" bind:this={inputElement} class="bg-black px-2 py-1 font-normal text-white" />
 			</div>
 		</div>
 	</body>
-	<!-- <footer class="flex h-[16%] w-full items-center justify-center border bg-[#000069] text-white">
-		<div class="flex h-32 w-[99%] items-center justify-center border border-x-4 border-white">
-			<div class="flex h-[65%] w-full flex-col gap-x-96 bg-black px-6 text-nowrap">
-            	 <div class="flex flex-row justify-between w-full mb-1">
-                    <p class="text-3xl tracking-widest">&#x2191&#x2193=Change line</p>
-                    <p class="text-3xl tracking-widest">&ltEnter&gt=Complete Entry</p>
-                    <p class="text-3xl tracking-widest">ESC=Exit Entry</p>
-                </div>
-                <div class="flex flex-row justify-between w-full">
-                    <p class="text-3xl tracking-widest">&ltTab&gt=Move Highlight</p>
-                    <p></p>
-                    <p></p>
-                </div>
-			</div>
-		</div>
-	</footer> -->
 	<footer class="flex h-[16%] w-full items-center justify-center border bg-[#000069] text-white">
 		<div class="flex h-32 w-[99%] items-center justify-center border border-x-4 border-white">
 			<div class="flex h-[65%] w-full items-end justify-start gap-x-96 bg-black px-6">
@@ -105,6 +89,22 @@
 			</div>
 		</div>
 	</footer>
+	<!-- <footer class="flex h-[16%] w-full items-center justify-center border bg-[#000069] text-white">
+		<div class="flex h-32 w-[99%] items-center justify-center border border-x-4 border-white">
+			<div class="flex h-[65%] w-full flex-col gap-x-96 text-nowrap bg-black px-6">
+				<div class="mb-1 flex w-full flex-row justify-between">
+					<p class="text-3xl tracking-widest">&#x2191&#x2193=Change line</p>
+					<p class="text-3xl tracking-widest">&ltEnter&gt=Complete Entry</p>
+					<p class="text-3xl tracking-widest">ESC=Exit Entry</p>
+				</div>
+				<div class="flex w-full flex-row justify-between">
+					<p class="text-3xl tracking-widest">&ltTab&gt=Move Highlight</p>
+					<p></p>
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</footer> -->
 </section>
 
 <style>
--- src/routes/serverRoom/systemBootTimeOut/+page.svelte
+++ src/routes/serverRoom/systemBootTimeOut/+page.svelte
@@ -1,40 +1,33 @@
 <script>
 	let active = 0;
-	const values = ['[S]', '<UEFI>', '<UEFI>', '<Disabled>', '<Disabled>' , 'disabled'];
-	const information = ['All functions on System boot timeout', 'Test System booting', 'change fast boot', 'Diagonise system booting', 'Override booting','Reset system booting'];
+	const values = ['[S]', '<UEFI>', '<UEFI>', '<Disabled>', '<Disabled>', 'disabled'];
+	const information = ['All functions on System boot timeout', 'Test System booting', 'change fast boot', 'Diagonise system booting', 'Override booting', 'Reset system booting'];
 	let showModal = writable(new Array(values.length).fill(false));
-	
-	import { writable } from 'svelte/store';
+
 	import Modal from '$lib/components/LIR/Modal.svelte';
 	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 	import { randomMessage } from '../randomMessage';
 
-	const modalOptions = [
-		{'[S]' : ['Normal Timeout', {'<Test>' : ['value one' , 'value two']}, 'Fast Boot', '<Diagnosis log>', '<Manual Override>', 'System Reset']},
-		{'<UEFI>' : ['safe mode', 'user mode', 'fast mode', 'dev mode']},
-		{'<Disabled>' : ['System test' , 'Server test' , 'Diagnosis test']},
-		{'<Enabled>' : ['Enable' , 'Disable']},
-		{'<UEFI>' : []},
-		{'<Disabled>' : []},
-		]
+	const modalOptions = [{ '[S]': ['Normal Timeout', { '<Test>': ['value one', 'value two'] }, 'Fast Boot', '<Diagnosis log>', '<Manual Override>', 'System Reset'] }, { '<UEFI>': ['safe mode', 'user mode', 'fast mode', 'dev mode'] }, { '<Disabled>': ['System test', 'Server test', 'Diagnosis test'] }, { '<Enabled>': ['Enable', 'Disable'] }, { '<UEFI>': [] }, { '<Disabled>': [] }];
 	/**
 	 * @type {({ '[S]': string[]; '<UEFI>'?: undefined; '<Disabled>'?: undefined; '<Enabled>'?: undefined; } | { '<UEFI>': string[]; '[S]'?: undefined; '<Disabled>'?: undefined; '<Enabled>'?: undefined; } | { '<Disabled>': string[]; '[S]'?: undefined; '<UEFI>'?: undefined; '<Enabled>'?: undefined; } | { '<Enabled>': string[]; '[S]'?: undefined; '<UEFI>'?: undefined; '<Disabled>'?: undefined; })[]}
 	 */
-	let modalStack = [] // contains objects of modal options
+	let modalStack = []; // contains objects of modal options
 
 	onMount(() => {
 		//@ts-expect-error
 		function handleKeyPress(event) {
 			event.preventDefault();
 			if (event.key === 'Escape') {
-				if ($showModal[active] === true){
+				if ($showModal[active] === true) {
 					modalStack.pop();
 					if (modalStack.length > 1) {
 						currentModal = modalStack[modalStack.length - 1];
 					} else {
 						$showModal[active] = false;
 					}
-					modalZIndex -= 1
+					modalZIndex -= 1;
 				} else {
 					window.location.href = '/serverRoom';
 				}
@@ -54,16 +47,17 @@
 				}
 			} else if (event.key === 'Enter') {
 				if ($showModal[active]) {
-					if (typeof(tempArrValues[modalIndex]) === 'object') {
+					if (typeof tempArrValues[modalIndex] === 'object') {
 						modalStack.push(tempArrValues[modalIndex]);
 						currentModal = tempArrValues[modalIndex];
-					} else if (typeof(tempArrValues[modalIndex]) === 'string') {
+					} else if (typeof tempArrValues[modalIndex] === 'string') {
 						// do nothing just copy the  value
 					}
 					modalZIndex += 1;
 					modalIndex = 0;
 				} else {
-					modalStack.push(modalOptions[active])
+					//@ts-expect-error
+					modalStack.push(modalOptions[active]);
 					$showModal[active] = true;
 				}
 				const valueToCopy = randomMessage();
@@ -77,7 +71,7 @@
 				navigator.clipboard
 					.writeText(valueToCopy)
 					.then(() => alert(`Value copied`)) // value not shown cuz let the players navigate multiple times in search of the code
-					.catch((error) => console.error('Unable to copy value:', error));				
+					.catch((error) => console.error('Unable to copy value:', error));
 			}
 		}
 
@@ -102,12 +96,12 @@
 	});
 
 	// states to keep track of the modal
-	$: modalZIndex = 0
-	$: modalIndex = 0
-	$: currentModal = modalOptions[active]
-	$: title= Object.keys(currentModal)[0]
-	$: tempArrValues = Object.values(currentModal)[0]
-	$: arrValues = updateArrValues(tempArrValues)
+	$: modalZIndex = 0;
+	$: modalIndex = 0;
+	$: currentModal = modalOptions[active];
+	$: title = Object.keys(currentModal)[0];
+	$: tempArrValues = Object.values(currentModal)[0];
+	$: arrValues = updateArrValues(tempArrValues);
 
 	/**
 	 * @param {string | any[]} tempArrValues
@@ -116,9 +110,9 @@
 		console.log(tempArrValues);
 		let arrValues = [];
 		for (let i = 0; i < tempArrValues.length; i++) {
-			if (typeof(tempArrValues[i]) === 'object') {
+			if (typeof tempArrValues[i] === 'object') {
 				arrValues.push(Object.keys(tempArrValues[i])[0]);
-			} else if (typeof(tempArrValues[i]) === 'string') {
+			} else if (typeof tempArrValues[i] === 'string') {
 				arrValues.push(tempArrValues[i]);
 			}
 		}
@@ -126,10 +120,10 @@
 	}
 </script>
 
-<section class="relative h-screen w-full bg-[#9c9a9d] font-IBM cursor-none">
+<section class="relative h-screen w-full cursor-none bg-[#9c9a9d] font-IBM">
 	{#if $showModal[active]}
 		{#if modalStack.length > 0}
-			<Modal title={title} arrValues={arrValues} {showModal} message={"LOL you are not authorized to change it"} curr={active} />
+			<Modal {title} {arrValues} {showModal} message={'LOL you are not authorized to change it'} curr={active} />
 		{/if}
 	{/if}
 	<header class="flex h-[10%] w-full items-center justify-center bg-[#000069]">
@@ -138,7 +132,7 @@
 		</div>
 	</header>
 	<body class="flex h-[74%] w-full flex-wrap bg-inherit text-3xl font-[600] tracking-normal text-black">
-		<div class="flex basis-1/3 flex-col p-10 gap-y-4">
+		<div class="flex basis-1/3 flex-col gap-y-4 p-10">
 			<p>Functions</p>
 			<p>Test</p>
 			<p>Fast Boot</p>
